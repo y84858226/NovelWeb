aa81ea94b2af9cf6bee5c0bf2f968b372dfc6e36
836127990@qq.com <nanku520>
2018-05-08 23:17:05 +0800

    测试seajs和jquery

---
+++ src/main/webapp/index.js
@@ -0,0 +1,6 @@
+define(function (require, exports, module) {
+    var $ = require('jquery');
+    $('#test').on("click",function () {
+        alert("seajs & jquery 测试成功！")
+    })
+})
---
+++ src/main/webapp/js/cmd.config.js
@@ -0,0 +1,5 @@
+seajs.config({
+    alias:{
+        'jquery':'jquery-3.3.1.min.js',
+    }
+});
