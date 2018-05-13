49d5facfa843b64cc066ceec5d5fdb428895f897
Administrator <Administrator@YRS0Z1V711WZWO8>
2018-05-13 21:42:27 +0800

    封装了爬虫方法

--- src/main/java/com/novel/background/controller/CrawlerController.java
+++ src/main/java/com/novel/background/controller/CrawlerController.java
@@ -1,6 +1,5 @@
 package com.novel.background.controller;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,6 +8,7 @@ import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -81,55 +81,150 @@ public class CrawlerController {
 	 * 获取小说的全部信息
 	 */
 	public static void getNovelLink(String url) {
-		// 设置爬虫的页面
-		JsoupUtil jsoupUtil = new JsoupUtil(url);
-		Document doc = jsoupUtil.getDoc();
-		// 获取全部的link
-		Elements elementsA = doc.select("a");
-		//存放类型页的link
-		Set<String> novelTypeSet = new HashSet<String>();
-		// 设置小说类型页的正则
-		Pattern typeLink = Pattern.compile("^//www.biquge5200.cc/[A-Za-z]+/$");
-		// 循环访问全部a标签
-		for (Element elementa : elementsA) {
-			String aLink = elementa.attr("href");
-			Matcher matcher = typeLink.matcher(aLink);
-			boolean b = matcher.matches();
-			if (b) {
-				//是否开启字符串替换 变为标准的http://
-				if(true) {
-					aLink=aLink.replace("//", "https://");
-				}
-				novelTypeSet.add(aLink);
-			}
-		}
-		
-//		for (String string : novelTypeSet) {
-//			System.out.println(string);
-//		}
-		
-		//存放小说的link
-		Set<String> novelSet = new HashSet<String>();
-		// 读取每一个类型页
+		/**
+		 * 第一步 查找小说列表页
+		 */
+		// 存放类型页的link
+		Set<String> novelTypeSet = getHtmlAttr(url, null, "a", "all", "href", "www.biquge5200.cc/[A-Za-z]+/$",
+				"https://",0);
+		/**
+		 * 第二步 查找小说页
+		 */
+		// 存放小说的link
 		for (String novelTypeLink : novelTypeSet) {
-			jsoupUtil = new JsoupUtil(novelTypeLink);
-			doc = jsoupUtil.getDoc();
-			//获取类型页里面的全部link
-			elementsA = doc.select("a");
-			//设置小说的正则
-			Pattern novelLink = Pattern.compile("^https://www.biquge5200.cc/\\d+_\\d+/$");
-			// 循环访问类型页的全部a标签
-			for (Element elementa : elementsA) {
-				String aLink = elementa.attr("href");
-				Matcher matcher = novelLink.matcher(aLink);
-				boolean b = matcher.matches();
-				if (b) {
-					novelSet.add(aLink);
+			Set<String> novelSet = getHtmlAttr(novelTypeLink, null, "a", "all", "href", "www.biquge5200.cc/\\d+_\\d+/$",
+					"https://",0);
+			System.out.println("类型页:" + novelTypeLink + ",共抓取小说：" + novelSet.size() + "本");
+
+			// 访问每一个小说页面
+			for (String novelLink : novelSet) {
+				JsoupUtil jsoupUtil = new JsoupUtil(novelLink);
+				Document doc = jsoupUtil.getDoc();
+				// 小说地址
+				System.out.println("url:" + novelLink);
+
+				// 小说名称
+				Set<String> nameSet = getHtmlAttr(null, doc, "#info h1", "0", "html", "[\\s\\S]*", "",0);
+				for (String name : nameSet) {
+					System.out.println("小说:" + name);
 				}
+
+				// 作者
+				Set<String> authorSet = getHtmlAttr(null, doc, "#info p", "0", "html", "([^：]+)$", "",0);
+				for (String author : authorSet) {
+					System.out.println("作者:" + author);
+				}
+
+				// 类型名称
+				Set<String> typeNameSet = getHtmlAttr(null, doc, ".con_top a", "2", "html", "[\\s\\S]*", "",0);
+				for (String typeName : typeNameSet) {
+					System.out.println("类型名称:" + typeName);
+				}
+
+				// 表述
+				Set<String> descriptionSet = getHtmlAttr(null, doc, "#intro p", "0", "html", "[\\s\\S]*", "",0);
+				for (String description : descriptionSet) {
+					System.out.println("描述:" + description);
+				}
+
+				// 主图地址
+				Set<String> mainImageSet = getHtmlAttr(null, doc, "#fmimg img", "0", "src", "[\\s\\S]*", "",0);
+				for (String mainImage : mainImageSet) {
+					System.out.println("主图地址:" + mainImage);
+				}
+				// 全部章节
+				Set<String> chapterSet = getHtmlAttr(null, doc, "#list dd", "all", "html", "[\\s\\S]*", "",0);
+				for (String chapterA : chapterSet) {
+					Document chapterDoc = Jsoup.parse(chapterA);
+					Set<String> chapterLinkSet = getHtmlAttr(null, chapterDoc, "a", "0", "href", "[\\s\\S]*", "",0);
+					for (String chapterLink : chapterLinkSet) {
+						System.out.println("章节地址:" + chapterLink);
+					}
+
+					Set<String> chapterNumSet = getHtmlAttr(null, chapterDoc, "a", "0", "html", "第(.*)章", "",1);
+					for (String chapterNum : chapterNumSet) {
+						System.out.println("章节编号:" + chapterNum);
+					}
+
+					Set<String> chapterNameSet = getHtmlAttr(null, chapterDoc, "a", "0", "html", "[\\s\\S]*", "",0);
+					for (String chapterName : chapterNameSet) {
+						System.out.println("章节名称:" + chapterName);
+					}
+					break;
+				}
+
+				break;
 			}
-			//记录日志
+			break;
 		}
-		System.out.println("共抓取小说："+novelSet.size()+"本");
 	}
 
+	/**
+	 * 
+	 * @param url
+	 *            不为空就创建新的爬虫
+	 * @param doc
+	 *            可以传入doc作为爬虫数据
+	 * @param select
+	 *            选择器
+	 * @param num
+	 *            all为全部标签，也可以单独指定第num个标签
+	 * @param attrName
+	 *            标签的属性，可以设定html属性
+	 * @param reg
+	 *            获取自己想要的结果
+	 * @param appendResult
+	 *            为自己的结果做一个格式化，弥补正则的不足
+	 * @param groupNum
+	 *            设置读取哪一组的数据
+	 * @return
+	 */
+	public static Set<String> getHtmlAttr(String url, Document doc, String select, String num, String attrName,
+			String reg, String appendResult, int groupNum) {
+		if (url != null) {
+			JsoupUtil jsoupUtil = new JsoupUtil(url);
+			doc = jsoupUtil.getDoc();
+		}
+		// 存放正则后的的attr
+		Set<String> set = new HashSet<String>();
+		/**
+		 * 获取全部属性
+		 */
+		if (num.equals("all")) {
+			// 获取全部的attr
+			Elements elements = doc.select(select);
+			// 设置正则
+			Pattern pattern = Pattern.compile(reg);
+			// 循环访问全部attr标签
+			for (Element element : elements) {
+				String attr = null;
+				if (attrName.equals("html")) {
+					attr = element.html();
+				} else {
+					attr = element.attr(attrName);
+				}
+				Matcher matcher = pattern.matcher(attr);
+				if (matcher.find()) {
+					set.add(appendResult + matcher.group(groupNum));
+				}
+			}
+		} else {
+			/**
+			 * 获取第num个属性
+			 */
+			Element element = doc.select(select).get(Integer.parseInt(num));
+			String attr = null;
+			if (attrName.equals("html")) {
+				attr = element.html();
+			} else {
+				attr = element.attr(attrName);
+			}
+			Pattern pattern = Pattern.compile(reg);
+			Matcher matcher = pattern.matcher(attr);
+			if (matcher.find()) {
+				set.add(appendResult + matcher.group(groupNum));
+			}
+		}
+		return set;
+	}
 }
