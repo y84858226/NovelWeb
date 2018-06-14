229b96efa3f43aa352032c57456b8b8befbf93cb
Yan Hua <huayanh@amazon.com>
2018-06-14 13:47:24 +0800

    添加启动错误页面过滤

--- src/main/java/com/novel/Ssmspringboot2Application.java
+++ src/main/java/com/novel/Ssmspringboot2Application.java
@@ -1,14 +1,30 @@
 package com.novel;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.ErrorPageFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement//开启事务管理
+@EnableTransactionManagement // 开启事务管理
 public class Ssmspringboot2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Ssmspringboot2Application.class, args);
-    }
-    
+	public static void main(String[] args) {
+		SpringApplication.run(Ssmspringboot2Application.class, args);
+	}
+
+	@Bean
+	public ErrorPageFilter errorPageFilter() {
+		return new ErrorPageFilter();
+	}
+
+	@Bean
+	public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(filter);
+		filterRegistrationBean.setEnabled(false);
+		return filterRegistrationBean;
+	}
 }
